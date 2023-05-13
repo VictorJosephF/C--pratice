8d0335a33e0adcdffaec9958314c32ba68956c27
VICTOR JOSEPH FARIA <68386487+VictorJosephF@users.noreply.github.com>
2023-05-13 15:49:40 -0300

    Questão2.cs

---
+++ Questão2.cs
@@ -0,0 +1,16 @@
+// See https://aka.ms/new-console-template for more information
+namespace Q2 {
+    class program {
+
+        public static void Main(String[] args)
+    {      
+            Console.Write("Digite um numero: ");
+            string input = Console.ReadLine();
+            int num = int.Parse(input); 
+
+            Console.WriteLine("O numero digitado foi: "+ num);
+    }
+
+ }
+}
+    
